c1c8f9653f904a46aa47b8b96325044a1fafe0a1
burnout156 <brunoferreira378@gmail.com>
2020-04-07 19:39:01 -0300

    Revert "Revert "1 commit""
    This reverts commit 2f0c6768ec367f57f2f6cbe787f165aba88e21a0.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int velocity;
+    public int coins;
+    public float x;
+    public float y;
+    public Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        if(velocity <= 0)
+        {
+            velocity = 200;
+        }
+
+        UpdateCoinText();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        Move(x);
+    }
+
+    public void Move(float x)
+    {
+        if (x != 0 || y != 0)
+        {
+            rigidBody.velocity = new Vector2(x, y) * Time.deltaTime * velocity;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "coin")
+        {
+            coins++;
+            Destroy(collision.gameObject);
+            UpdateCoinText();
+        }
+    }
+
+    public void UpdateCoinText()
+    {
+        GameObject.Find("CoinText").GetComponent<Text>().text = "Coin: " + coins;
+    }
+}
