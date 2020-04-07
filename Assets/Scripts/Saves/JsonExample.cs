d3e8c3d5dd83d9c0adc15dc5fb25200ba0e64db5
burnout156 <brunoferreira378@gmail.com>
2020-04-07 19:30:02 -0300

    1 commit

---
+++ Assets/Scripts/Saves/JsonExample.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public struct SaveStructure
+{
+    public PlayerStatus player;
+}
+
+public class JsonExample : MonoBehaviour
+{
+    public static SaveStructure MainSave;
+    public static string filePath;
+    public static string folderPath;
+    public static string data;
+
+    private void Awake()
+    {        
+        folderPath = Application.dataPath + "/LocalSave"; //caminho da pasta
+        filePath = Application.dataPath + "/LocalSave" + "/" + "Save" + ".json"; //caminho do arquivo       
+    }
+
+    void Start()
+    {
+        /*
+        AutoSave();
+        MainSave = JsonUtility.FromJson<SaveStructure>(Load());       
+        */
+    }
+
+    public void Save(Player player)
+    {
+        if (!Directory.Exists(folderPath)) //se a pasta do save não existir, será criada uma nova pasta
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        if (!File.Exists(filePath)) //se o arquivo de save não existir, será criado 
+        {
+            MainSave.player = new PlayerStatus(player.coins);
+        }
+
+        string json = JsonUtility.ToJson(MainSave); //passar uma estrutura com variáveis para uma string
+        File.WriteAllText(filePath, json);
+        print("Arquivo salvo com sucesso: " + json);
+        GameObject.FindObjectOfType<Player>().UpdateCoinText();
+    }
+
+    public void AutoSaving()
+    {
+        if (!Directory.Exists(folderPath)) //se a pasta do save não existir, será criada uma nova pasta
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        if (!File.Exists(filePath)) //se o arquivo de save não existir, será criado
+        {
+            MainSave.player = new PlayerStatus(GameObject.FindObjectOfType<Player>().coins);
+        }
+
+        string json = JsonUtility.ToJson(MainSave); //passar uma estrutura com variáveis para uma string
+        File.WriteAllText(filePath, json);
+    }
+
+    public void Load() //também pode funcionar como um auto carregamento, apenas precisando colocar no Start
+    {
+        if (File.Exists(filePath))
+        {
+            data = File.ReadAllText(filePath);
+            MainSave = JsonUtility.FromJson<SaveStructure>(data);
+            print("Arquivo lido com sucesso: " + data);
+            GameObject.FindObjectOfType<Player>().coins = MainSave.player.coins;
+            GameObject.FindObjectOfType<Player>().UpdateCoinText();
+        }
+
+        else
+        {
+            print("Arquivo não existe: " + data);
+        }
+    }
+
+    public void Delete()
+    {
+        if (Directory.Exists(folderPath)) //se a pasta do save existir, será deletada
+        {
+            Directory.Delete(folderPath, true);
+            GameObject.FindObjectOfType<Player>().UpdateCoinText();
+            print("Arquivo deletado com sucesso");
+        }
+
+        else
+        {
+            print("File Not Exist");
+        }
+    }
+}
---
+++ Assets/Scripts/Saves/PlayerPrefsExample.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsExample : MonoBehaviour
+{
+
+    public Player player;
+
+    private void Start()
+    {
+        player = GameObject.FindObjectOfType<Player>();
+    }
+
+    public void Save() 
+    {
+        PlayerPrefs.SetInt("coins", player.coins); 
+        print("File Prefab Save Successfully: " + PlayerPrefs.GetInt("coins"));
+        //local onde salva: (Windows + R, e digita: regedit) HKEY_CURRENT_CONFIG\Software[company name][product name] key
+    }
+
+    public void Load()
+    {
+        if(PlayerPrefs.HasKey("coins")) //Se existir a chave com nome de moedas(coins), ele carrega o "save"
+        {
+            player.coins = PlayerPrefs.GetInt("coins");
+            GameObject.FindObjectOfType<Player>().UpdateCoinText();
+            print("File Prefab Read Successfully: " + PlayerPrefs.GetInt("coins"));
+        }
+    }
+
+    public void Delete()
+    {
+        PlayerPrefs.DeleteAll();
+        GameObject.FindObjectOfType<Player>().UpdateCoinText();
+        print("File Prefab Delete Successfully:");
+    }
+}
